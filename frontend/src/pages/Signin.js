8ec5396d2150e2958cc530547937a31e5dbf893a
Danish <dan71ish@gmail.com>
2023-07-22 10:17:01 +0530

    Revert "remove bg"
    This reverts commit 566aaaecb62dd3db8fcd109f4104da7ff38ec6bd.

--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -2,9 +2,9 @@ const Signin = {
     after_render: () => {},
     render: () => {
         return `
-        <div class="w-full h-[78vh] flex justify-center items-center">
+        <div class="w-full h-[78vh] bg-red-200 flex justify-center items-center">
         <div class="form w-[40%] flex justify-center items-center">
-        <form id="signin-form" class="bg-white p-3 border-zinc-400 rounded border w-1/2">
+        <form id="signin-form" class="bg-white p-3 rounded border w-1/2">
         <h1  class="text-3xl font-semibold mb-3">Sign in </h1>
         <label for="email" class="text-2xl font-medium  my-2 block">Email</label>
         <input type="email" name="email" id="email" class="border border-black block w-full rounded py-2 px-2 focus:outline-none focus:border-2 transition-all duration-75"/>
